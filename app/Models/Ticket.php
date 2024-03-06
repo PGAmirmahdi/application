03c95f1b2d473b8ed5cec0af02c1e373b1788361
Mostafa <shrytzadh@gmail.com>
2024-03-06 09:09:40 +0330

    change time format

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,11 @@ class Ticket extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 
     const STATUS = [
         'pending' => 'درحال بررسی',
