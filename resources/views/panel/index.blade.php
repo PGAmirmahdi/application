c93c133e4abcb00088423debc80374828a9e856d
Mostafa <shrytzadh@gmail.com>
2024-03-14 09:27:12 +0330

    panel statistics

--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -36,7 +36,7 @@
                                         </div>
                                     </div>
                                     <span class="font-size-13">کاربران</span>
-                                    <h2 class="mb-0 ml-auto font-weight-bold text-success primary-font line-height-30">0</h2>
+                                    <h2 class="mb-0 ml-auto font-weight-bold text-success primary-font line-height-30">{{ \App\Models\User::count() }}</h2>
                                 </div>
                             </div>
                         </div>
@@ -47,11 +47,11 @@
                                 <div class="d-flex align-items-center mb-3">
                                     <div>
                                         <div class="icon-block icon-block-sm bg-secondary icon-block-floating mr-2">
-                                            <i class="fa fa-users"></i>
+                                            <i class="fa fa-list"></i>
                                         </div>
                                     </div>
-                                    <span class="font-size-13">مشتریان</span>
-                                    <h2 class="mb-0 ml-auto font-weight-bold text-secondary primary-font line-height-30">0</h2>
+                                    <span class="font-size-13">محصولات</span>
+                                    <h2 class="mb-0 ml-auto font-weight-bold text-secondary primary-font line-height-30">{{ \App\Models\Product::count() }}</h2>
                                 </div>
                             </div>
                         </div>
@@ -62,11 +62,11 @@
                                 <div class="d-flex align-items-center mb-3">
                                     <div>
                                         <div class="icon-block icon-block-sm bg-info icon-block-floating mr-2">
-                                            <i class="fa fa-p"></i>
+                                            <i class="fa fa-shopping-cart"></i>
                                         </div>
                                     </div>
-                                    <span class="font-size-13">محصولات</span>
-                                    <h2 class="mb-0 ml-auto font-weight-bold text-info primary-font line-height-30">0</h2>
+                                    <span class="font-size-13">سفارشات</span>
+                                    <h2 class="mb-0 ml-auto font-weight-bold text-info primary-font line-height-30">{{ \App\Models\Order::count() }}</h2>
                                 </div>
                             </div>
                         </div>
@@ -77,15 +77,45 @@
                                 <div class="d-flex align-items-center mb-3">
                                     <div>
                                         <div class="icon-block icon-block-sm bg-primary icon-block-floating mr-2">
-                                            <i class="fa fa-shopping-cart"></i>
+                                            <i class="fa fa-dollar"></i>
                                         </div>
                                     </div>
-                                    <span class="font-size-13">سفارش مشتری</span>
-                                    <h2 class="mb-0 ml-auto font-weight-bold text-primary primary-font line-height-30">0</h2>
+                                    <span class="font-size-13">تراکنش ها</span>
+                                    <h2 class="mb-0 ml-auto font-weight-bold text-primary primary-font line-height-30">{{ \App\Models\Payment::count() }}</h2>
                                 </div>
                             </div>
                         </div>
                     </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                        <div class="card border mb-0">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center mb-3">
+                                    <div>
+                                        <div class="icon-block icon-block-sm bg-primary icon-block-floating mr-2">
+                                            <i class="fa fa-message"></i>
+                                        </div>
+                                    </div>
+                                    <span class="font-size-13">تیکت ها</span>
+                                    <h2 class="mb-0 ml-auto font-weight-bold text-primary primary-font line-height-30">{{ \App\Models\Ticket::count() }}</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                    <div class="card border mb-0">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center mb-3">
+                                <div>
+                                    <div class="icon-block icon-block-sm bg-primary icon-block-floating mr-2">
+                                        <i class="fa fa-comment"></i>
+                                    </div>
+                                </div>
+                                <span class="font-size-13">نظرات</span>
+                                <h2 class="mb-0 ml-auto font-weight-bold text-primary primary-font line-height-30">{{ \App\Models\Comment::count() }}</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
