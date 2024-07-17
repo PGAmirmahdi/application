97a2c170f0b1cef76ad2856a7455d448effdb1d7
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-07-17 10:18:07 +0330

    fixed some bugs"

--- resources/views/panel/coupons/index.blade.php
+++ resources/views/panel/coupons/index.blade.php
@@ -29,7 +29,7 @@
                     @foreach($coupons as $key => $coupon)
                         <tr>
                             <td>{{ ++$key }}</td>
-                            <td>{{ $coupon->user->name . " " . $coupon->user->family}}</td>
+                            <td>@if($coupon->user) {{ $coupon->user->name }} @else مختص به کاربری نیست @endif</td>
                             <td>{{ $coupon->title }}</td>
                             <td>{{ $coupon->code }}</td>
                             <td>{{ $coupon->limit }}</td>
