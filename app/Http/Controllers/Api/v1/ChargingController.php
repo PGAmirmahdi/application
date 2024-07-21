a14fdaf4dd72b85634e02acd4f1c7b3b5dfe1b4f
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-07-21 15:20:53 +0330

    fixed rial and toman in dargah

--- app/Http/Controllers/Api/v1/ChargingController.php
+++ app/Http/Controllers/Api/v1/ChargingController.php
@@ -173,12 +173,12 @@ class ChargingController extends Controller
                 'message' => 'تراکنشی با این شناسه موجود نیست',
             ], 404); // Added status code 404 for not found
         }
-
+        $TomantoRial=$payment->amount  * 10;
         // Prepare data for ZarinPal API
         $data = [
             "merchant_id" => env('MERCHANT_ID'),
             "authority" => $authority,
-            "amount" => $payment->amount,
+            "amount" => $TomantoRial,
         ];
 
         $jsonData = json_encode($data);
