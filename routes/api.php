dd8e65473cc9bd44a4668bf1bc309dc0fc35dffa
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-07-21 15:08:08 +0330

    fixed some bugs

--- routes/api.php
+++ routes/api.php
@@ -81,7 +81,7 @@ Route::prefix('v1')->group(function (){
     Route::get('getCharging', [ChargingController::class, 'getCharging']);
     Route::post('Charge', [ChargingController::class, 'Charge']);
     Route::post('Charging-verify', [ChargingController::class, 'verify']);
-    Route::get('get-verify-url', [ChargingController::class,'getVerifyUrl']);
+    Route::post('get-verify-url', [ChargingController::class,'getVerifyUrl']);
 
     Route::middleware('auth:sanctum')->group(function (){
 
