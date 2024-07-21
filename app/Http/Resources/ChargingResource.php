c6997d38f5a21f2d31d3f3385ff4d77c77a85500
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-07-21 16:07:15 +0330

    fixed bug of status in payments

--- app/Http/Resources/ChargingResource.php
+++ app/Http/Resources/ChargingResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Payment;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ChargingResource extends JsonResource
@@ -23,13 +24,17 @@ class ChargingResource extends JsonResource
         return [
             'charging_id' => $this->charging_id,
             'amount' => $this->amount,
-            'status' => $this->status,
+            'pay-status' => $this->status,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
             'type'=>$this->type,
             'description'=>$this->description,
             'wallet_id'=>$this->wallet_id,
-            'tracking_code'=>$this->tracking_code
+            'tracking_code'=>$this->tracking_code,
+            'pay_status' => $this->payment->status,
+            'pay_status_text' => Payment::STATUS[$this->payment->status],
+            'pay_status_color' => Payment::STATUS_COLOR[$this->payment->status],
+            'authority' => $this->payment ? 'https://www.zarinpal.com/pg/StartPay/'.$this->payment->authority : null,
         ];
     }
 }
