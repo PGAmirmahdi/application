d561299a71f60d757652799d5b28b9294c2c7d1d
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-07-22 10:36:46 +0330

    fixed bug

--- resources/views/panel/payments/index.blade.php
+++ resources/views/panel/payments/index.blade.php
@@ -118,18 +118,20 @@
                                 // Handle different error codes and success states
                                 if (res.error_code == 100 || res.error_code == 101) {
                                     btn_check.parent().siblings('.status')[0].innerHTML = `<span class="badge badge-success">موفق</span>`;
-                                } else if (res.error_code == -51) {
-                                    // Handle the session expired case
-                                    btn_check.parent().siblings('.status')[0].innerHTML = `<span class="badge badge-danger">پرداخت ناموفق</span>`;
-                                } else {
+                                }
+                            else {
                                     // Handle other error codes or unexpected responses
                                     btn_check.parent().siblings('.status')[0].innerHTML = `<span class="badge badge-danger">ناموفق</span>`;
                                 }
                                 btn_check.removeAttr('disabled');
                             },
-                            error: function () {
+                            error: function (res) {
                                 btn_check.parent().siblings('.status')[0].innerHTML = `<span class="badge badge-danger">ناموفق</span>`;
                                 btn_check.removeAttr('disabled');
+                            if (res.error_code == -51) {
+                                    // Handle the session expired case
+                                    btn_check.parent().siblings('.status')[0].innerHTML = `<span class="badge badge-danger">پرداخت ناموفق</span>`;
+                            }
                             }
                         });
                     },
