4ed82d5e17189ec9f74a2a1d2e02756ee4d36756
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-07-22 14:58:20 +0330

    may fixed

--- resources/views/panel/payments/index.blade.php
+++ resources/views/panel/payments/index.blade.php
@@ -52,8 +52,8 @@
                     @foreach($payments as $key => $payment)
                         <tr>
                             <td>{{ ++$key }}</td>
-                                <td>@if($payment->types == false){{ $payment->order->user->name }}@elseif($payment->types == true){{ $payment->wallets->user->name }}@endif</td>
-                                <td>@if($payment->types == false){{ $payment->order->user->family }}@elseif($payment->types == true){{ $payment->wallets->user->family }}@endif</td>
+                                <td>@if($payment->types == false){{ $payment->order->user->name }}@elseif($payment->types == true){{ $payment->wallets->users->name }}@endif</td>
+                                <td>@if($payment->types == false){{ $payment->order->user->family }}@elseif($payment->types == true){{ $payment->wallets->users->family }}@endif</td>
                             <td>{{ number_format($payment->amount) }}</td>
                             <td class="status">
                                 @if($payment->status == 'success')
