416e5ea921a114e1071b204c0d6201cb684e9632
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-07-22 10:15:34 +0330

    fixed auth

--- routes/api.php
+++ routes/api.php
@@ -80,7 +80,7 @@ Route::prefix('v1')->group(function (){
     // Charging
     Route::get('getChargings', [ChargingController::class, 'getChargings']);
     Route::post('Charge', [ChargingController::class, 'Charge']);
-    Route::post('Charging-verify', [ChargingController::class, 'verify'])->middleware('auth:sanctum');
+    Route::post('Charging-verify', [ChargingController::class, 'verify']);
     Route::post('get-verify-url', [ChargingController::class,'getVerifyUrl'])->middleware('auth:sanctum');
     Route::get('getCharging', [ChargingController::class, 'getCharging']);
     Route::get('getCharges', [ChargingController::class, 'getCharges'])->middleware('auth:sanctum');
