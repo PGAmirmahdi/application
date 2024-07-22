a20d3a9ff64a574f015dd0849cd4ff75d0f98c78
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-07-22 10:35:14 +0330

    test

--- resources/views/panel/payments/index.blade.php
+++ resources/views/panel/payments/index.blade.php
@@ -118,7 +118,7 @@
                                 // Handle different error codes and success states
                                 if (res.error_code == 100 || res.error_code == 101) {
                                     btn_check.parent().siblings('.status')[0].innerHTML = `<span class="badge badge-success">موفق</span>`;
-                                } else if (res.error_code == "51-") {
+                                } else if (res.error_code == -51) {
                                     // Handle the session expired case
                                     btn_check.parent().siblings('.status')[0].innerHTML = `<span class="badge badge-danger">پرداخت ناموفق</span>`;
                                 } else {
