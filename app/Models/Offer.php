9a52076efed562d2b81fe20c6bcc11ba4f8d565b
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-07-31 16:30:31 +0330

    fixed

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -12,6 +12,6 @@ class Offer extends Model
 
     public function products()
     {
-        return $this->belongsto(Product::class,'offer_id');
+        return $this->belongsto(Product::class,'product_id');
     }
 }
