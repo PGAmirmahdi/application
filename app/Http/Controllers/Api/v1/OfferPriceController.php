7cf6ed5ff0012d13bcf52b04354623120799237a
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-08-01 12:27:44 +0330

    fixed

--- app/Http/Controllers/Api/v1/OfferPriceController.php
+++ app/Http/Controllers/Api/v1/OfferPriceController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Offer;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class OfferPriceController extends Controller
 {
@@ -16,5 +18,46 @@ class OfferPriceController extends Controller
         } else {
             return response()->json(['success' => false]);
         }
+    }
+
+    public function getOffer(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'product_id' => 'required|exists:products,id'
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validate->errors()->messages(),
+            ]);
+        }
+        $product_id= $request->product_id;
+        $item = Offer::where('offers.id', $product_id)
+            ->join('products', 'offers.product_id', '=', 'products.id')
+            ->select([
+                'offers.product_id as product_id',
+                'products.title as product_title',
+                'offers.description as description',
+                'offers.percentage as percentage',
+                'offers.price_before as price_before',
+                'offers.price_after as price_after',
+            ])
+            ->first();
+
+        if (!$item) {
+            return response()->json([
+                'success' => false,
+                'data' => [
+
+                ]
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'اطلاعات داده شد',
+            'data' => [$item]
+        ]);
     }
 }
