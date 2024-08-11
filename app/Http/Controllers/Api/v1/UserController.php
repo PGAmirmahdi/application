53ce2975640061178f42c8589ba3512915d66a07
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-08-11 14:18:19 +0330

    fixed bug

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -167,7 +167,7 @@ class UserController extends Controller
                 'phone_code' => $code,
                 'phone_expire' => now()->addMinutes(2)
             ]);
-            sendSMS(240154, $user->phone, [$code, $user->name . ' ' .  $user->family]);
+            sendSMS(240154, $user->phone, [$user->name . ' ' .  $user->family,$code]);
         }
 
         return response()->json([
