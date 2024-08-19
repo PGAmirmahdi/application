e74731f6b5180459339b9cb31259b9b33af6df2d
Amirmahdi Asadi <PGAmirmahdi@gmail.com>
2024-08-19 14:40:25 +0330

    fixed

--- app/Models/DeviceInfo.php
+++ app/Models/DeviceInfo.php
@@ -10,7 +10,7 @@ class DeviceInfo extends Model
     use HasFactory;
 
     protected $guarded = [];
-
+    protected $table = 'deviceinfo';
     protected $fillable = [
         'From',
         'Browser_Name',
